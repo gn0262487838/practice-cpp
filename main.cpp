f1432dcaf025a9256e3643b457596cb8a6d8b45c
Chenkol_baby <49306125+gn0262487838@users.noreply.github.com>
2019-05-08 01:26:20 +0800

    the key to two .cpp compile
    /*
    兩個.cpp要執行在VS code running是預設一個.cpp，
    way1:
    因此可直接#include "fibon.cpp"進來，但需要在fibon.h中加入#ifndef...#define...#endif
    不然會重覆定義fibon喔!!!
    
    way2:
    或者在terminal(powershell)一起編譯兩個.cpp
    Copyright (C) Microsoft Corporation. All rights reserved.
    
    PS C:\Users\gn026\OneDrive\Documents\test cpp> g++ main.cpp fibon.cpp  編譯兩個cpp中間只需要空格就好，就會產生a.exe執行檔(windows)
    PS C:\Users\gn026\OneDrive\Documents\test cpp> ./a.exe   執行檔案前要加上./
    */

--- main.cpp
+++ main.cpp
@@ -3,7 +3,20 @@
 
 #include "fibon.h"
 
-#include "fibon.cpp" //兩個.cpp要執行先要include(in VSc)並在fibon.h中加入#ifndef...#define...#endif
+// #include "fibon.cpp" 
+/*
+兩個.cpp要執行在VS code running是預設一個.cpp，
+way1:
+因此可直接#include "fibon.cpp"進來，但需要在fibon.h中加入#ifndef...#define...#endif
+不然會重覆定義fibon喔!!!
+
+way2:
+或者在terminal(powershell)一起編譯兩個.cpp
+Copyright (C) Microsoft Corporation. All rights reserved.
+
+PS C:\Users\gn026\OneDrive\Documents\test cpp> g++ main.cpp fibon.cpp  編譯兩個cpp中間只需要空格就好，就會產生a.exe執行檔(windows)
+PS C:\Users\gn026\OneDrive\Documents\test cpp> ./a.exe   執行檔案前要加上./
+*/
 
 using namespace std;
 
