27258e247fc20c32a6a11983692d29e09aef7fae
Chenkol_baby <49306125+gn0262487838@users.noreply.github.com>
2019-05-07 13:53:57 +0800

    Create fibon.h

---
+++ fibon.h
@@ -0,0 +1,16 @@
+
+
+//fibonacci seqence
+
+class fibon{
+    public:
+    fibon(int);
+    int fun_fibon();
+    int fibon_set(int); 
+
+    private:
+    int fibon_num1;
+    int fibon_num2;
+    int fibon_num3;
+
+};
