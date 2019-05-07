1a03d9d80b1d83f48a0fb14a0dc7dc5e69336e93
Chenkol_baby <49306125+gn0262487838@users.noreply.github.com>
2019-05-07 15:00:24 +0800

    Update fibon.cpp

--- fibon.cpp
+++ fibon.cpp
@@ -1,23 +1,33 @@
-
 #include "fibon.h"
 
-fibon::fibon(int User_var){
-    fibon_set(User_var);
+fibon::fibon(int n){
+    fibon_set(n);
 }
 
-int fibon::fibon_set(int User_var){
-    return User_var;
+int fibon::fibon_set(int m){
+    User_var = m;
+    return User_var; 
 }
 
 int fibon::fun_fibon(){
     fibon_num1 = 1;
-    fibon_num2 = 1; 
-    for(int i=3;i<=(User_var + 2);i++){
-        fibon_num3 = fibon_num1 + fibon_num2;
-        fibon_num1 = fibon_num2;
-        fibon_num2 = fibon_num3;
+    fibon_num2 = 1;
+    if(User_var == 1){
+        return fibon_num3 = 1;
     }
-
-    return fibon_num3;
+    else if(User_var == 2){
+        return fibon_num3 = 1;
+    }
+    else{
+        
+        for(int i=3;i<=User_var;i++){
+            fibon_num3 = fibon_num1 + fibon_num2;
+            fibon_num1 = fibon_num2;
+            fibon_num2 = fibon_num3;
+        }
+        return fibon_num3;
+    }
 }
+
+
 
